662330b7c761332f37320427878dae7e8c030edc
xiaomeng <sunxm@xingyuanauto.com>
2017-11-02 17:14:36 +0800

    Initial Commit

---
+++ Algorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithm
+//
+//  Created by 小萌 on 2017/11/2.
+//  Copyright © 2017年 小萌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorithm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithm
+//
+//  Created by 小萌 on 2017/11/2.
+//  Copyright © 2017年 小萌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
